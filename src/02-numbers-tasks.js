1aa1b65c6054fc81cd35e3534b888de5e9af9621
Arina1204 <arinachirets@gmail.com>
2026-05-02 20:31:55 +0300

    part 2 done

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -18,7 +18,7 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea( width, height ) {
+function getRectangleArea(width, height) {
   return width * height;
 }
 
@@ -33,8 +33,8 @@ function getRectangleArea( width, height ) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference( radius ) {
-  return 2 * radius * 3.14;
+function getCircleCircumference(radius) {
+  return 2 * radius * Math.PI;
 }
 
 /**
@@ -49,8 +49,8 @@ function getCircleCircumference( radius ) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage( value1, value2 ) {
-return (value1 + value2) / 2
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -68,8 +68,8 @@ return (value1 + value2) / 2
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints( x1, y1, x2, y2 ) {
- return Math.sqrt( (x2-x1 )**2 + (y2-y1)**2);
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
@@ -84,8 +84,8 @@ function getDistanceBetweenPoints( x1, y1, x2, y2 ) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot( a, b ) {
-  return (0 - b)/a ;
+function getLinearEquationRoot(a, b) {
+  return (0 - b) / a;
 }
 
 /**
@@ -106,12 +106,12 @@ function getLinearEquationRoot( a, b ) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2 ) {
-  const dot = x1 * x2 + y1*y2;
-  const a = Math.sqrt(x1**2 + y1**2);
-  const b = Math.sqrt(x2**2 + y2**2);
-const cos = dot / Math.abs( a * b )
-return Math.acos(cos);
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dot = x1 * x2 + y1 * y2;
+  const a = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const b = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const cos = dot / Math.abs(a * b);
+  return Math.acos(cos);
 }
 
 /**
@@ -126,7 +126,7 @@ return Math.acos(cos);
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit( value ) {
+function getLastDigit(value) {
   return Math.abs(value % 10);
 }
 
@@ -141,7 +141,7 @@ function getLastDigit( value ) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString( value ) {
+function parseNumberFromString(value) {
   return Number(value);
 }
 
@@ -158,8 +158,8 @@ function parseNumberFromString( value ) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal( a, b, c ) {
-  return Math.sqrt(a**2 + b**2 + c**2);
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -179,9 +179,9 @@ function getParallelepipedDiagonal( a, b, c ) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen( num, pow ) {
-      const factor = 10 ** pow;  
-    return Math.round(num / factor) * factor;
+function roundToPowerOfTen(num, pow) {
+  const factor = 10 ** pow;
+  return Math.round(num / factor) * factor;
 }
 
 /**
@@ -201,13 +201,13 @@ function roundToPowerOfTen( num, pow ) {
  *   16 => false
  *   17 => true
  */
-function isPrime( n ) {
-if (n <= 1) return false;
-if (n<= 3) return true;
-   for (let i = 2; i < n; i++) {
-        if (n % i === 0) return false;
-    }
-    return true;
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -225,9 +225,9 @@ if (n<= 3) return true;
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber( value, def ) {
-const num = Number(value);
-return isNaN(num) ? def : num;
+function toNumber(value, def) {
+  const num = Number(value);
+  return Number.isNaN(num) ? def : num;
 }
 
 module.exports = {
